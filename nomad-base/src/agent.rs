6727df8151c2ef732b28555b9c25682b1cc6b956
Luke Tchang <ltchang@stanford.edu>
2022-02-10 21:33:55 -0800

    fix: move macro to macro.rs

--- nomad-base/src/agent.rs
+++ nomad-base/src/agent.rs
@@ -2,8 +2,7 @@ use crate::{
     cancel_task,
     metrics::CoreMetrics,
     settings::{IndexSettings, Settings},
-    BaseError, CachingHome, CachingReplica, ChannelBase, ContractSyncMetrics, IndexDataTypes,
-    NomadDB,
+    BaseError, CachingHome, CachingReplica, ContractSyncMetrics, IndexDataTypes, NomadDB,
 };
 use async_trait::async_trait;
 use color_eyre::{eyre::WrapErr, Result};
