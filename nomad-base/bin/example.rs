d4d02aa44fb65937eaaf5a420a4b67f10fc690ff
Luke Tchang <ltchang@stanford.edu>
2022-03-20 16:37:27 -0600

    chore: put example bin agent back

--- nomad-base/bin/example.rs
+++ nomad-base/bin/example.rs
@@ -1,14 +1,32 @@
 use color_eyre::Result;
 
-use nomad_base::{NomadAgent, Settings};
+use nomad_base::{decl_settings, AgentSecrets, AgentSettingsBlock, NomadAgent};
+
+// Example settings block containing addition agent-specific fields
+#[derive(Debug, Clone, serde::Deserialize)]
+pub struct ExampleAgentSettingsBlock;
+
+// Show how to derive agent-specific settings block from config and secrets
+impl AgentSettingsBlock for ExampleAgentSettingsBlock {
+    fn from_config_and_secrets(
+        _home_network: &str,
+        _config: &nomad_xyz_configuration::NomadConfig,
+        _secrets: &AgentSecrets,
+    ) -> Self {
+        Self
+    }
+}
+
+// Generate ExampleSettings which includes base and agent-specific settings
+decl_settings!(Example, ExampleAgentSettingsBlock,);
 
 /// An example main function for any agent that implemented Default
-async fn _example_main<OA>(settings: Settings) -> Result<()>
+async fn _example_main<NA>(settings: ExampleSettings) -> Result<()>
 where
-    OA: NomadAgent<Settings = Settings> + Sized + 'static,
+    NA: NomadAgent<Settings = ExampleSettings> + Sized + 'static,
 {
     // Instantiate an agent
-    let oa = OA::from_settings(settings).await?;
+    let oa = NA::from_settings(settings).await?;
     oa.start_tracing(oa.metrics().span_duration())?;
 
     // Use the agent to run a number of replicas
@@ -17,15 +35,14 @@ where
 
 /// Read settings from the config file and set up reporting and logging based
 /// on the settings
-// #[allow(dead_code)]
-// fn setup() -> Result<Settings> {
-// TODO: add back base settings block
-// color_eyre::install()?;
+#[allow(dead_code)]
+fn setup() -> Result<ExampleSettings> {
+    color_eyre::install()?;
 
-// let settings = Settings::new()?;
+    let settings = ExampleSettings::new()?;
 
-// Ok(settings)
-// }
+    Ok(settings)
+}
 
 #[allow(dead_code)]
 fn main() -> Result<()> {
