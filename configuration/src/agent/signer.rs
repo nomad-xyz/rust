ce35296bc8c6ac0a53f16b007c3527726a11519f
James <james@prestwi.ch>
2022-03-28 17:27:20 -0700

    feature: single-field deser for signerconf and connection

--- configuration/src/agent/signer.rs
+++ configuration/src/agent/signer.rs
@@ -1,8 +1,11 @@
 use nomad_types::HexString;
+use serde::{
+    de::{self},
+    Deserialize,
+};
 
 /// Ethereum signer types
-#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
-#[serde(tag = "type", rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub enum SignerConf {
     /// A local hex key
     HexKey {
@@ -17,9 +20,80 @@ pub enum SignerConf {
         /// The AWS region
         region: String,
     },
-    #[serde(other)]
     /// Assume node will sign on RPC calls
     Node,
+}
+
+impl<'de> serde::Deserialize<'de> for SignerConf {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(field_identifier, rename_all = "lowercase")]
+        enum AwsField {
+            Id,
+            Region,
+        }
+
+        struct SignerConfVisitor;
+
+        impl<'de> de::Visitor<'de> for SignerConfVisitor {
+            type Value = SignerConf;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str(
+                    "A 32-byte 0x-prefixed hex-key, OR an aws key id and regior OR nothing",
+                )
+            }
+
+            fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                let key = HexString::<64>::from_string(v).map_err(de::Error::custom)?;
+                Ok(SignerConf::HexKey { key })
+            }
+
+            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
+            where
+                A: de::MapAccess<'de>,
+            {
+                let mut id: Option<String> = None;
+                let mut region: Option<String> = None;
+
+                while let Some(key) = map.next_key()? {
+                    match key {
+                        AwsField::Id => {
+                            if id.is_some() {
+                                return Err(de::Error::duplicate_field("id"));
+                            }
+                            id = Some(map.next_value()?);
+                        }
+                        AwsField::Region => {
+                            if region.is_some() {
+                                return Err(de::Error::duplicate_field("region"));
+                            }
+                            region = Some(map.next_value()?);
+                        }
+                    }
+                }
+                let id = id.ok_or_else(|| de::Error::missing_field("id"))?;
+                let region = region.ok_or_else(|| de::Error::missing_field("region"))?;
+                Ok(SignerConf::Aws { id, region })
+            }
+
+            fn visit_none<E>(self) -> Result<Self::Value, E>
+            where
+                E: de::Error,
+            {
+                Ok(SignerConf::Node)
+            }
+        }
+
+        const VARIANTS: &'static [&'static str] = &["hexkey", "aws", "node"];
+        deserializer.deserialize_enum("SignerConf", VARIANTS, SignerConfVisitor)
+    }
 }
 
 impl Default for SignerConf {
--- configuration/src/chains/ethereum.rs
+++ configuration/src/chains/ethereum.rs
@@ -1,8 +1,9 @@
 //! Ethereum/EVM configuration types
 
+use std::str::FromStr;
+
 /// Ethereum connection configuration
-#[derive(Debug, serde::Deserialize, Clone, PartialEq)]
-#[serde(tag = "type", rename_all = "camelCase")]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Connection {
     /// HTTP connection details
     Http {
@@ -14,6 +15,36 @@ pub enum Connection {
         /// Fully qualified string to connect to
         url: String,
     },
+}
+
+impl Connection {
+    fn from_string(s: String) -> eyre::Result<Self> {
+        if s.starts_with("http://") || s.starts_with("https://") {
+            Ok(Self::Http { url: s })
+        } else if s.starts_with("wss://") || s.starts_with("ws://") {
+            Ok(Self::Ws { url: s })
+        } else {
+            eyre::bail!("Expected http or websocket URI")
+        }
+    }
+}
+
+impl FromStr for Connection {
+    type Err = eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Self::from_string(s.to_owned())
+    }
+}
+
+impl<'de> serde::Deserialize<'de> for Connection {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Self::from_string(s).map_err(serde::de::Error::custom)
+    }
 }
 
 impl Default for Connection {
