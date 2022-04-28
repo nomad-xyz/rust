d6de7ccd72f818bee301349d1f0327d43002a8f3
Luke Tchang <ltchang@stanford.edu>
2022-04-28 13:52:28 -0700

    feat(gelato): finish gelato bindings for sending relay, getting supported chains, and task status

---
+++ gelato-relay/bin/test.rs
@@ -0,0 +1,14 @@
+use gelato_relay::*;
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), reqwest::Error> {
+    let chains = get_gelato_relay_chains().await?;
+    println!("Relay chains: {:?}", chains);
+
+    let task_status =
+        get_task_status("0xeefc20b15402c30ead95d572034532c7097488726a6582d3d6674971e9d97879")
+            .await?;
+    println!("Task status: {:?}", task_status);
+
+    Ok(())
+}
--- gelato-relay/src/types.rs
+++ gelato-relay/src/types.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -9,6 +9,80 @@ pub struct RelayRequest {
     pub relayer_fee: String,
 }
 
-pub struct RelayTransaction {
-    pub task_id: String
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RelayResponse {
+    pub task_id: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RelayChainsResponse {
+    pub relays: Vec<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TaskStatusResponse {
+    data: Vec<TaskStatus>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TaskStatus {
+    pub service: String,
+    pub chain: String,
+    pub task_id: String,
+    pub task_state: TaskState,
+    #[serde(rename = "created_at")]
+    pub created_at: String, // date
+    pub last_check: Option<String>,
+    pub execution: Option<Execution>,
+    pub last_execution: String, // date
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Execution {
+    pub status: String,
+    pub transaction_hash: String,
+    pub block_number: usize,
+    pub created_at: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Check {
+    pub task_state: TaskState,
+    pub message: Option<String>,
+    #[serde(rename = "created_at")]
+    pub created_at: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Payload {
+    pub to: String,
+    pub data: String,
+    pub fee_data: FeeData,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FeeData {
+    pub gas_price: usize,
+    pub max_fee_per_gas: usize,
+    pub max_priority_fee_per_gas: usize,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub enum TaskState {
+    CheckPending,
+    ExecPending,
+    ExecSuccess,
+    ExecReverted,
+    WaitingForConfirmation,
+    Blacklisted,
+    Cancelled,
+    NotFound,
 }
