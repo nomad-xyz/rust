58aeb091e045481b3e2219b09a4fcc475a639e0b
Luke Tchang <ltchang@stanford.edu>
2022-04-28 15:58:26 -0700

    refactor(gelato): make client object

--- gelato-relay/src/lib.rs
+++ gelato-relay/src/lib.rs
@@ -3,45 +3,65 @@ use types::*;
 
 use color_eyre::eyre::Result;
 
-const RELAY_URL: &str = "https://relay.gelato.digital";
+const DEFAULT_URL: &str = "https://relay.gelato.digital";
 
-pub async fn send_relay_transaction(
-    chain_id: usize,
-    dest: &str,
-    data: &str,
-    token: &str,
-    relayer_fee: &str,
-) -> Result<RelayResponse, reqwest::Error> {
-    let params = RelayRequest {
-        dest: dest.to_owned(),
-        data: data.to_owned(),
-        token: token.to_owned(),
-        relayer_fee: relayer_fee.to_owned(),
-    };
-
-    let url = format!("{}/relays/{}", RELAY_URL, chain_id);
-    let res = reqwest::Client::new()
-        .post(url)
-        .json(&params)
-        .send()
-        .await?;
-
-    res.json().await
+pub struct GelatoClient {
+    url: String,
 }
 
-pub async fn is_chain_supported(chain_id: usize) -> Result<bool, reqwest::Error> {
-    let supported_chains = get_gelato_relay_chains().await?;
-    Ok(supported_chains.contains(&chain_id.to_string()))
+impl Default for GelatoClient {
+    fn default() -> Self {
+        Self::new(DEFAULT_URL.to_owned())
+    }
 }
 
-pub async fn get_gelato_relay_chains() -> Result<Vec<String>, reqwest::Error> {
-    let url = format!("{}/relays", RELAY_URL);
-    let res = reqwest::get(url).await?;
-    Ok(res.json::<RelayChainsResponse>().await?.relays)
-}
+impl GelatoClient {
+    pub fn new(url: String) -> Self {
+        Self { url }
+    }
 
-pub async fn get_task_status(task_id: &str) -> Result<TaskStatusResponse, reqwest::Error> {
-    let url = format!("{}/tasks/{}", RELAY_URL, task_id);
-    let res = reqwest::get(url).await?;
-    Ok(res.json().await?)
+    pub async fn send_relay_transaction(
+        &self,
+        chain_id: usize,
+        dest: &str,
+        data: &str,
+        token: &str,
+        relayer_fee: &str,
+    ) -> Result<RelayResponse, reqwest::Error> {
+        let params = RelayRequest {
+            dest: dest.to_owned(),
+            data: data.to_owned(),
+            token: token.to_owned(),
+            relayer_fee: relayer_fee.to_owned(),
+        };
+
+        let url = format!("{}/relays/{}", &self.url, chain_id);
+        let res = reqwest::Client::new()
+            .post(url)
+            .json(&params)
+            .send()
+            .await?;
+
+        res.json().await
+    }
+
+    pub async fn is_chain_supported(&self, chain_id: usize) -> Result<bool, reqwest::Error> {
+        let supported_chains = self.get_gelato_relay_chains().await?;
+        Ok(supported_chains.contains(&chain_id.to_string()))
+    }
+
+    pub async fn get_gelato_relay_chains(&self) -> Result<Vec<String>, reqwest::Error> {
+        let url = format!("{}/relays", &self.url);
+        let res = reqwest::get(url).await?;
+        Ok(res.json::<RelayChainsResponse>().await?.relays)
+    }
+
+    pub async fn get_task_status(
+        &self,
+        task_id: &str,
+    ) -> Result<TaskStatusResponse, reqwest::Error> {
+        let url = format!("{}/tasks/{}", &self.url, task_id);
+        let res = reqwest::get(url).await?;
+        Ok(res.json().await?)
+    }
 }
