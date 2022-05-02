78a381265fe20202e723309b25b67490db66bd50
Luke Tchang <ltchang@stanford.edu>
2022-05-02 15:56:40 -0700

    fix(gelato-client): add ethers middleware to gelato client for gas estimation

--- chains/nomad-ethereum/src/gelato.rs
+++ chains/nomad-ethereum/src/gelato.rs
@@ -1,7 +1,7 @@
 use ethers::providers::Middleware;
 use gelato_relay::{GelatoClient, RelayResponse, TaskState};
 use nomad_core::Signers;
-use std::marker::PhantomData;
+use std::sync::Arc;
 
 /*
 {
@@ -28,8 +28,10 @@ pub(crate) const ACCEPTABLE_STATES: [TaskState; 4] = [
 /// Gelato client for submitting txs to single chain
 #[derive(Debug, Clone)]
 pub struct SingleChainGelatoClient<M> {
-    /// Base client
-    pub client: GelatoClient,
+    /// Gelato client
+    pub gelato: GelatoClient,
+    /// Ethers client (for estimating gas)
+    pub eth_client: Arc<M>,
     /// Sponsor signer
     pub sponsor: Signers,
     /// Chain id
@@ -38,30 +40,32 @@ pub struct SingleChainGelatoClient<M> {
     pub fee_token: String,
     /// Transactions are of high priority
     pub is_high_priority: bool,
-    /// Unused
-    _middleware: PhantomData<M>,
 }
 
-impl<M: Middleware + 'static> SingleChainGelatoClient<M> {
+impl<M> SingleChainGelatoClient<M>
+where
+    M: Middleware + 'static,
+{
     /// Get reference to base client
-    pub fn client(&self) -> &GelatoClient {
-        &self.client
+    pub fn gelato(&self) -> &GelatoClient {
+        &self.gelato
     }
 
     /// Instantiate single chain client with default Gelato url
     pub fn with_default_url(
+        eth_client: Arc<M>,
         sponsor: Signers,
         chain_id: usize,
         fee_token: String,
         is_high_priority: bool,
     ) -> Self {
         Self {
-            client: GelatoClient::default(),
+            gelato: GelatoClient::default(),
+            eth_client,
             sponsor,
             chain_id,
             fee_token,
             is_high_priority,
-            _middleware: Default::default(),
         }
     }
 
@@ -73,7 +77,7 @@ impl<M: Middleware + 'static> SingleChainGelatoClient<M> {
         gas_limit: usize,
     ) -> Result<RelayResponse, reqwest::Error> {
         let relayer_fee = self
-            .client
+            .gelato()
             .get_estimated_fee(
                 self.chain_id,
                 &self.fee_token,
@@ -82,7 +86,7 @@ impl<M: Middleware + 'static> SingleChainGelatoClient<M> {
             )
             .await?;
 
-        self.client
+        self.gelato()
             .send_relay_transaction(self.chain_id, dest, data, &self.fee_token, relayer_fee)
             .await
     }
