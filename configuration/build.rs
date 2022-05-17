6c80abc2ad625ff2ce038ab00465fda36d87a45d
James <james@prestwi.ch>
2022-05-17 13:23:26 -0700

    feature: build.rs fetches json from github pages

--- configuration/build.rs
+++ configuration/build.rs
@@ -1,21 +1,46 @@
-use std::io::Write;
+use std::{fs, io::Write, path::PathBuf};
+use tokio;
 
 const DEFINITIONS: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/data/definitions.ts"));
 const TYPEDEFS: &str = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/data/types.rs"));
 
 const OUTPUT_FILE: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/wasm/types.rs");
 
-fn main() -> eyre::Result<()> {
-    println!(
-        "cargo:rerun-if-changed={}",
-        concat!(env!("CARGO_MANIFEST_DIR"), "/data/definitions.ts")
-    );
-    println!(
-        "cargo:rerun-if-changed={}",
-        concat!(env!("CARGO_MANIFEST_DIR"), "/data/types.rs")
-    );
+const CONFIG_BASE_URI: &str = "https://nomad-xyz.github.io/config";
+const ENVS: &[&str] = &["development", "staging", "production"];
+const CONFIG_BASE_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/configs");
 
-    let mut f = std::fs::OpenOptions::new()
+fn env_json(env: &str) -> String {
+    format!("{}.json", env)
+}
+
+fn config_uri(env: &str) -> String {
+    format!("{}/{}", CONFIG_BASE_URI, env_json(env))
+}
+
+fn config_path(env: &str) -> PathBuf {
+    PathBuf::from(CONFIG_BASE_DIR).join(env_json(env))
+}
+
+async fn fetch_config(env: &str) -> eyre::Result<String> {
+    let uri = config_uri(env);
+    Ok(reqwest::get(uri).await?.text().await?)
+}
+
+fn store_config(env: &str, contents: &str) -> eyre::Result<()> {
+    let mut f = fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .truncate(true)
+        .open(config_path(env))
+        .unwrap();
+
+    f.write_all(contents.as_ref())?;
+    Ok(())
+}
+
+fn gen_wasm_bindgen() -> eyre::Result<()> {
+    let mut f = fs::OpenOptions::new()
         .write(true)
         .truncate(true)
         .open(OUTPUT_FILE)
@@ -37,6 +62,27 @@ const _: &'static str = r#""###
     writeln!(f, r###""#;"###)?;
     writeln!(f)?;
     f.write_all(TYPEDEFS.as_ref())?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    println!(
+        "cargo:rerun-if-changed={}",
+        concat!(env!("CARGO_MANIFEST_DIR"), "/data/definitions.ts")
+    );
+    println!(
+        "cargo:rerun-if-changed={}",
+        concat!(env!("CARGO_MANIFEST_DIR"), "/data/types.rs")
+    );
+
+    for env in ENVS {
+        let text = fetch_config(env).await?;
+        store_config(env, &text)?;
+    }
+
+    gen_wasm_bindgen()?;
 
     Ok(())
 }
