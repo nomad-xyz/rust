b45b6ed07d81a06793daa7a42242fcb609db84c2
Matthew Smith <m@lattejed.com>
2022-05-19 23:22:35 +0700

    Expand doc for from_env

--- configuration/src/traits/env.rs
+++ configuration/src/traits/env.rs
@@ -7,8 +7,10 @@ pub trait EnvOverridable {
 /// Implemented by structs that are built from environment variables (signers,
 /// connections, etc)
 pub trait FromEnv {
-    /// Optionally load self from env vars. Return None if any necessary env var
-    /// is missing.
+    /// Optionally load self from env vars.
+    /// Accepts a `default_prefix` which will be looked for if `prefix` isn't found.
+    /// If both are present, `prefix` has precedence over `default_prefix`.
+    /// Return None if *any* necessary env var is missing.
     fn from_env(prefix: &str, default_prefix: Option<&str>) -> Option<Self>
     where
         Self: Sized;
