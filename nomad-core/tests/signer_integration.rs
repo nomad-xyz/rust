7e5de63da4d62542cda7196dc6225365ee23e979
James <james@prestwi.ch>
2022-05-23 12:25:57 -0700

    fix: update for new from env

--- nomad-core/tests/signer_integration.rs
+++ nomad-core/tests/signer_integration.rs
@@ -4,7 +4,7 @@ use nomad_xyz_configuration::{agent::SignerConf, FromEnv};
 
 #[tokio::test]
 async fn signer_auths() {
-    if let Some(signer_conf) = SignerConf::from_env("TEST_KMS") {
+    if let Some(signer_conf) = SignerConf::from_env("TEST_KMS", None) {
         let signer = Signers::try_from_signer_conf(&signer_conf).await.unwrap();
         let message = "hello world";
         signer.sign_message(message).await.unwrap();
