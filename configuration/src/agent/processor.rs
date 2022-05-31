50fe0ad69818e2dea8b07be087c84922fd12d554
Matthew Smith <m@lattejed.com>
2022-06-01 01:50:01 +0700

    Add environment var override test

--- configuration/src/agent/processor.rs
+++ configuration/src/agent/processor.rs
@@ -54,3 +54,53 @@ decl_env_overrides!(Processor {self, {
         self.s3 = Some(S3Config { bucket, region })
     }
 }});
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use nomad_test::test_utils;
+    use std::{env, str::FromStr};
+
+    #[test]
+    #[serial_test::serial]
+    fn it_overrides_config_from_env() {
+        test_utils::run_test_with_env_sync("../fixtures/env.test-agents", move || {
+            let mut config = ProcessorConfig::default();
+            config.load_env_overrides();
+
+            let hashes = HashSet::from([
+                H256::from_str(
+                    "0x1111111111111111111111111111111111111111111111111111111111111111",
+                )
+                .unwrap(),
+                H256::from_str(
+                    "0x1111111111111111111111111111111111111111111111111111111111111112",
+                )
+                .unwrap(),
+                H256::from_str(
+                    "0x1111111111111111111111111111111111111111111111111111111111111113",
+                )
+                .unwrap(),
+            ]);
+            assert_eq!(config.allowed, Some(hashes.clone()));
+            assert_eq!(config.denied, Some(hashes));
+            assert_eq!(
+                config.subsidized_remotes,
+                HashSet::from([
+                    "chain1".to_string(),
+                    "chain3".to_string(),
+                    "chain2".to_string(),
+                ])
+            );
+            assert_eq!(
+                config.s3,
+                Some(S3Config {
+                    bucket: "aws-bucket".to_string(),
+                    region: "region-1".to_string(),
+                })
+            );
+            assert_eq!(config.interval, 999);
+            assert_eq!(config.enabled, true);
+        });
+    }
+}
