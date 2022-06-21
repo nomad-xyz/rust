8f9979f81a87654c729b05865d27ea8c1e0660d5
Matthew Smith <m@lattejed.com>
2022-06-22 01:16:56 +0700

    Make TxTranslator async and have it return errors

--- nomad-core/src/traits/transaction.rs
+++ nomad-core/src/traits/transaction.rs
@@ -1,9 +1,14 @@
-use crate::PersistedTransaction;
+use crate::{ChainCommunicationError, PersistedTransaction};
+use async_trait::async_trait;
 
 /// Interface for chain-agnostic to chain-specifc transaction translators
+#[async_trait]
 pub trait TxTranslator {
     type Transaction;
 
     /// Translate to chain-specific type
-    fn convert(&self, tx: PersistedTransaction) -> Self::Transaction;
+    async fn convert(
+        &self,
+        tx: PersistedTransaction,
+    ) -> Result<Self::Transaction, ChainCommunicationError>;
 }
