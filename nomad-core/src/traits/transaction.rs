803553222644f7d333d2a868bb117eb65d08b8be
Matthew Smith <m@lattejed.com>
2022-06-23 20:16:21 +0700

    Add TxForwarder and TxSender traits

--- nomad-core/src/traits/transaction.rs
+++ nomad-core/src/traits/transaction.rs
@@ -4,6 +4,7 @@ use async_trait::async_trait;
 /// Interface for chain-agnostic to chain-specifc transaction translators
 #[async_trait]
 pub trait TxTranslator {
+    /// Concrete transaction type
     type Transaction;
 
     /// Translate to chain-specific type
@@ -11,4 +12,18 @@ pub trait TxTranslator {
         &self,
         tx: PersistedTransaction,
     ) -> Result<Self::Transaction, ChainCommunicationError>;
+}
+
+/// Interface for forwarding between abstract and concrete contract instances
+#[async_trait]
+pub trait TxForwarder: Send + Sync + std::fmt::Debug {
+    /// Translate to chain-specific type
+    async fn forward(&self, tx: PersistedTransaction);
+}
+
+/// Interface for submitting PersistentTransaction to a contract
+#[async_trait]
+pub trait TxSender {
+    /// Translate to chain-specific type
+    async fn send(&self, tx: PersistedTransaction);
 }
