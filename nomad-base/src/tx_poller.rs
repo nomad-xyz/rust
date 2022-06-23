a12a8d548c11f76f47ae72a9a30ece71c1d00136
Matthew Smith <m@lattejed.com>
2022-06-23 19:29:56 +0700

    Update TxPoller to accept a forwarder trait object and forward new transactions

--- nomad-base/src/tx_poller.rs
+++ nomad-base/src/tx_poller.rs
@@ -1,7 +1,7 @@
 use crate::NomadDB;
 use color_eyre::Result;
-use nomad_core::{NomadEvent, PersistedTransaction, TxDispatchKind, TxOutcome};
-use std::time::Duration;
+use nomad_core::{NomadEvent, PersistedTransaction, TxForwarder};
+use std::{sync::Arc, time::Duration};
 
 const TX_STATUS_POLL_MS: u64 = 100;
 
@@ -9,28 +9,29 @@ const TX_STATUS_POLL_MS: u64 = 100;
 #[derive(Debug, Clone)]
 pub struct TxPoller {
     db: NomadDB,
+    contract: Arc<dyn TxForwarder>,
 }
 
 impl TxPoller {
     /// Create a new TxPoller with a DB ref
-    pub fn new(db: NomadDB) -> Self {
-        Self { db }
+    pub fn new(db: NomadDB, contract: Arc<dyn TxForwarder>) -> Self {
+        Self { db, contract }
+    }
+
+    /// Return the next tx that needs sending
+    fn next_transaction(&self) -> Option<PersistedTransaction> {
+        let mut iter = self.db.persisted_transaction_iterator();
+        iter.find(|tx| tx.confirm_event == NomadEvent::Dummy2)
     }
 
     /// Run the polling loop to send off new transactions
     pub async fn run(&self) -> Result<()> {
+        let contract = self.contract.clone();
         loop {
-            tokio::time::sleep(Duration::from_millis(TX_STATUS_POLL_MS)).await;
-
-            let iter = self.db.persisted_transaction_iterator();
-            for tx in iter {
-                match tx.confirm_event {
-                    NomadEvent::Dummy2 => {
-                        // send this off
-                    }
-                    _ => continue,
-                }
+            if let Some(tx) = self.next_transaction() {
+                contract.forward(tx).await;
             }
+            tokio::time::sleep(Duration::from_millis(TX_STATUS_POLL_MS)).await;
         }
     }
 }
