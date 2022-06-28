5bd313b38ea04b4c1e1725ddb38f707ac28fbaef
Matthew Smith <m@lattejed.com>
2022-06-28 20:01:13 +0700

    Make status method names unambiguous

--- nomad-core/src/traits/transaction.rs
+++ nomad-core/src/traits/transaction.rs
@@ -26,14 +26,18 @@ pub trait TxForwarder: Send + Sync + std::fmt::Debug {
 #[async_trait]
 pub trait TxEventStatus: std::fmt::Debug {
     /// Get status of transaction via contract state
-    async fn status(&self, tx: &PersistedTransaction)
-        -> Result<TxOutcome, ChainCommunicationError>;
+    async fn event_status(
+        &self,
+        tx: &PersistedTransaction,
+    ) -> Result<TxOutcome, ChainCommunicationError>;
 }
 
 /// Interface for checking tx status via contract state
 #[async_trait]
 pub trait TxContractStatus: std::fmt::Debug {
     /// Get status of transaction via contract state
-    async fn status(&self, tx: &PersistedTransaction)
-        -> Result<TxOutcome, ChainCommunicationError>;
+    async fn contract_status(
+        &self,
+        tx: &PersistedTransaction,
+    ) -> Result<TxOutcome, ChainCommunicationError>;
 }
