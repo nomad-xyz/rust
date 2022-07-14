a8231be3f41e5e8a20c5a57bf6bce0db4af5dc65
Matthew Smith <m@lattejed.com>
2022-07-14 16:36:20 +0700

    Stub out updated TxSender

--- nomad-base/src/tx_sender.rs
+++ nomad-base/src/tx_sender.rs
@@ -1,42 +1,56 @@
 use crate::NomadDB;
 use color_eyre::Result;
-use nomad_core::{NomadTxStatus, PersistedTransaction};
-use std::{sync::Arc, time::Duration};
+use nomad_core::PersistedTransaction;
+use tokio::{
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
+    task::JoinHandle,
+};
 
-const TX_STATUS_POLL_MS: u64 = 100;
-
-/// Transaction poller for forwarding PersistentTransaction to a concrete contract
-#[derive(Debug, Clone)]
+/// TxSender that receives outgoing transactions, polls for status and forwards to
+/// concrete contract impl to translate and submit transactions
+#[derive(Debug)]
 pub struct TxSender {
     db: NomadDB,
-    contract: Arc<dyn TxForwarder>,
+    in_sender: UnboundedSender<PersistedTransaction>,
+    in_receiver: Option<UnboundedReceiver<PersistedTransaction>>,
+    out_sender: UnboundedSender<PersistedTransaction>,
+    out_receiver: Option<UnboundedReceiver<PersistedTransaction>>,
 }
 
 impl TxSender {
-    /// Create a new TxPoller with a DB and contract ref
-    pub fn new(db: NomadDB, contract: Arc<dyn TxForwarder>) -> Self {
-        Self { db, contract }
-    }
-
-    /// Return the next tx that needs sending
-    fn next_transaction(&self) -> Option<PersistedTransaction> {
-        let mut iter = self.db.persisted_transaction_iterator();
-        iter.find(|tx| tx.confirm_event == NomadTxStatus::Dummy2)
-    }
-
-    /// Run the polling loop to send off new transactions
-    pub async fn run(&self) -> Result<()> {
-        let contract = self.contract.clone();
-        loop {
-            if let Some(mut tx) = self.next_transaction() {
-                tx.confirm_event = match contract.forward(tx.clone()).await {
-                    Ok(outcome) => outcome.into(),
-                    Err(error) => error.into(),
-                };
-                self.db.update_persisted_transaction(&tx)?;
-            }
-            tokio::time::sleep(Duration::from_millis(TX_STATUS_POLL_MS)).await;
+    /// Create a new TxPoller with a DB and an UnboundedReceiver
+    pub fn new(db: NomadDB) -> Self {
+        let (in_sender, in_receiver) = unbounded_channel();
+        let (out_sender, out_receiver) = unbounded_channel();
+        Self {
+            db,
+            in_sender,
+            in_receiver: Some(in_receiver),
+            out_sender,
+            out_receiver: Some(out_receiver),
         }
+    }
+
+    /// Take out_receiver for the run loop
+    pub fn take_out_receiver(&mut self) -> Option<UnboundedReceiver<PersistedTransaction>> {
+        self.out_receiver.take()
+    }
+
+    /// Clone in_sender for external use
+    pub fn in_sender(&self) -> UnboundedSender<PersistedTransaction> {
+        self.in_sender.clone()
+    }
+
+    /// Spawn run loop task
+    pub fn send_task(&mut self) -> Option<JoinHandle<Result<()>>> {
+        let mut in_receiver = self.in_receiver.take().unwrap();
+        Some(tokio::spawn(async move {
+            loop {
+                if let Ok(_tx) = in_receiver.try_recv() {
+                    unimplemented!()
+                }
+            }
+        }))
     }
 }
 
