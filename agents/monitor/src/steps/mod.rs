a8e439a8d5c5deb816439b4878f62478318a14a3
James <james@prestwi.ch>
2022-07-20 13:57:03 -0700

    feature: relay to process metrics

--- agents/monitor/src/steps/mod.rs
+++ agents/monitor/src/steps/mod.rs
@@ -1,3 +1,4 @@
 pub(crate) mod between;
 pub(crate) mod dispatch_wait;
+pub(crate) mod relay_wait;
 pub(crate) mod update_wait;
---
+++ agents/monitor/src/steps/relay_wait.rs
@@ -0,0 +1,98 @@
+use ethers::prelude::U64;
+use prometheus::Histogram;
+use tokio::time::Instant;
+
+use tracing::{info_span, Instrument};
+
+use crate::{bail_task_if, ProcessFaucet, ProcessSink, ProcessStep, RelayFaucet, RelaySink};
+
+pub struct RelayWaitMetrics {
+    timers: Histogram,
+    blocks: Histogram,
+}
+
+pub(crate) struct RelayWait {
+    relay_faucet: RelayFaucet,
+    process_faucet: ProcessFaucet,
+
+    network: String,
+    replica_of: String,
+    emitter: String,
+    metrics: RelayWaitMetrics,
+
+    relay_instant: Instant,
+    relay_block: U64,
+
+    relay_sink: RelaySink,
+    process_sink: ProcessSink,
+}
+
+impl std::fmt::Display for RelayWait {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "RelayToProcess latency for {}'s replica of {} @ {}",
+            self.network, self.replica_of, self.emitter,
+        )
+    }
+}
+
+impl ProcessStep for RelayWait {
+    fn spawn(mut self) -> crate::Restartable<Self>
+    where
+        Self: 'static + Send + Sync + Sized,
+    {
+        let span = info_span!(
+            "ProcessWait",
+            network = self.network.as_str(),
+            emitter = self.emitter.as_str(),
+        );
+        tokio::spawn(
+            async move {
+                loop {
+                    tokio::select! {
+                        biased;
+
+                        process_next = self.process_faucet.recv() => {
+                            bail_task_if!(
+                                process_next.is_none(),
+                                self,
+                                "inbound relay broke"
+                            );
+                            let process = process_next.expect("checked");
+                            let process_instant = tokio::time::Instant::now();
+                            let process_block = process.meta.block_number;
+                            bail_task_if!(
+                                self.process_sink.send(process).is_err(),
+                                self,
+                                "outbound relay broke",
+                            );
+                            let elapsed_ms = process_instant.saturating_duration_since(self.relay_instant).as_millis() as f64;
+                            let elapsed_blocks = process_block.saturating_sub(self.relay_block).as_u64() as f64;
+
+                            self.metrics.timers.observe(elapsed_ms);
+                            self.metrics.blocks.observe(elapsed_blocks);
+                        }
+                        relay_next = self.relay_faucet.recv() => {
+                            bail_task_if!(
+                                relay_next.is_none(),
+                                self,
+                                "inbound relay broke"
+                            );
+                            let relay = relay_next.expect("checked");
+                            self.relay_instant = tokio::time::Instant::now();
+                            self.relay_block = relay.meta.block_number;
+                            bail_task_if!(
+                                self.relay_sink.send(relay).is_err(),
+                                self,
+                                "outbound relay broke",
+                            );
+                        }
+
+                    }
+                }
+            }
+            .instrument(span),
+        )
+    }
+}
