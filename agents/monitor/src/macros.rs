f9083fe20b3e971fc379a0cd131e366208f73be2
James <james@prestwi.ch>
2022-07-27 19:21:34 +0200

    fix: prevent double logging recoverable bails

--- agents/monitor/src/macros.rs
+++ agents/monitor/src/macros.rs
@@ -7,8 +7,7 @@ macro_rules! bail_task_if {
     ($cond:expr, $self:ident, $err:expr) => {
         if $cond {
             let err = eyre::eyre!($err);
-            tracing::error!(task = %$self, err = %err, "Task failed");
-            return $crate::steps::TaskResult::Recoverable{task: $self, err};
+            return $crate::steps::TaskResult::Recoverable { task: $self, err };
         }
     };
 }
