406256f9521e8f5414da60f7d99dcb6459747665
James <james@prestwi.ch>
2022-07-27 00:54:19 -0700

    test: add recovery tests

--- agents/monitor/src/main.rs
+++ agents/monitor/src/main.rs
@@ -4,7 +4,7 @@ use nomad_ethereum::bindings::{
     home::{DispatchFilter, UpdateFilter},
     replica::{ProcessFilter, UpdateFilter as RelayFilter},
 };
-use std::{collections::HashMap, panic, sync::Arc};
+use std::{collections::HashMap, panic, sync::Arc, time::Duration};
 use steps::TaskResult;
 use tokio::{
     sync::mpsc::{UnboundedReceiver, UnboundedSender},
@@ -117,18 +117,114 @@ pub(crate) trait ProcessStep: std::fmt::Display {
                         if panic_res.is_err() {
                             tracing::trace!(
                                 task = task_description.as_str(),
-                                "Internal task cancelled"
+                                "Internal task cancelled",
                             );
                             break;
                         }
                         let p = panic_res.unwrap();
-                        tracing::error!("Internal task panicked");
+                        tracing::error!(task = task_description.as_str(), "Internal task panicked");
                         panic::resume_unwind(p);
                     }
                 };
 
+                tokio::time::sleep(Duration::from_secs(15)).await;
                 handle = again.spawn();
             }
         })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{steps::TaskResult, ProcessStep};
+
+    struct RecoverableTask;
+    impl std::fmt::Display for RecoverableTask {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "RecoverableTask")
+        }
+    }
+
+    impl ProcessStep for RecoverableTask {
+        fn spawn(self) -> crate::Restartable<Self>
+        where
+            Self: 'static + Send + Sync + Sized,
+        {
+            tokio::spawn(async move {
+                TaskResult::Recoverable {
+                    task: self,
+                    err: eyre::eyre!("This error was recoverable"),
+                }
+            })
+        }
+    }
+
+    #[tokio::test]
+    #[tracing_test::traced_test]
+    async fn test_recovery() {
+        let handle = RecoverableTask.run_until_panic();
+        tokio::time::sleep(std::time::Duration::from_secs(2)).await;
+        handle.abort();
+        let result = handle.await;
+
+        assert!(logs_contain("RecoverableTask"));
+        assert!(logs_contain("Restarting task"));
+        assert!(logs_contain("This error was recoverable"));
+        assert!(result.is_err() && result.unwrap_err().is_cancelled());
+    }
+
+    struct UnrecoverableTask;
+    impl std::fmt::Display for UnrecoverableTask {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "UnrecoverableTask")
+        }
+    }
+
+    impl ProcessStep for UnrecoverableTask {
+        fn spawn(self) -> crate::Restartable<Self>
+        where
+            Self: 'static + Send + Sync + Sized,
+        {
+            tokio::spawn(async move {
+                TaskResult::Unrecoverable(eyre::eyre!("This error was unrecoverable"))
+            })
+        }
+    }
+
+    #[tokio::test]
+    #[tracing_test::traced_test]
+    async fn test_unrecoverable() {
+        let handle = UnrecoverableTask.run_until_panic();
+        let result = handle.await;
+        assert!(logs_contain("UnrecoverableTask"));
+        assert!(logs_contain("Unrecoverable error encountered"));
+        assert!(logs_contain("This error was unrecoverable"));
+        assert!(result.is_ok());
+    }
+
+    struct PanicTask;
+    impl std::fmt::Display for PanicTask {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "PanicTask")
+        }
+    }
+
+    impl ProcessStep for PanicTask {
+        fn spawn(self) -> crate::Restartable<Self>
+        where
+            Self: 'static + Send + Sync + Sized,
+        {
+            tokio::spawn(async move { panic!("intentional panic :)") })
+        }
+    }
+
+    #[tokio::test]
+    #[tracing_test::traced_test]
+    async fn test_panic() {
+        let handle = PanicTask.run_until_panic();
+        let result = handle.await;
+        assert!(logs_contain("PanicTask"));
+        assert!(logs_contain("Internal task panicked"));
+        assert!(result.is_err() && result.unwrap_err().is_panic());
     }
 }
