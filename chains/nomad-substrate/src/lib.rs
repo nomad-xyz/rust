10ce39e8d52a74aa7511780835c5b3118873bbd5
Luke Tchang <ltchang@stanford.edu>
2022-08-18 13:02:17 -0400

    temp(nomad-substrate): saving stubs for dynamic tx/storage subxt

--- chains/nomad-substrate/src/lib.rs
+++ chains/nomad-substrate/src/lib.rs
@@ -3,9 +3,9 @@ pub use avail_subxt_config::{avail, AvailConfig};
 
 pub mod home;
 
-#[macro_use]
-pub mod macros;
+// #[macro_use]
+// pub mod macros;
 
 pub type SubstrateSigner<T: subxt::Config> = dyn subxt::tx::Signer<T> + Send + Sync;
 
-codegen_home!(Avail, avail, AvailConfig);
+// codegen_home!(Avail, avail, AvailConfig);
