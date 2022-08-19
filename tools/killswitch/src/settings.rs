206acf6e5cb7b5f7d6a1de58b0d039905cd7eca8
Matthew Smith <m@lattejed.com>
2022-08-19 20:40:04 +0700

    Add settings members and misc cleanup

--- tools/killswitch/src/settings.rs
+++ tools/killswitch/src/settings.rs
@@ -1,20 +1,26 @@
-use color_eyre::Result;
+use color_eyre::{eyre, Result};
+use nomad_core::Signers;
+use nomad_xyz_configuration::{AgentSecrets, NomadConfig};
+use std::{collections::HashMap, env};
 
 #[derive(Debug)]
-pub(crate) struct KillSwitchSettings {}
+pub(crate) struct KillSwitchSettings {
+    secrets: HashMap<String, AgentSecrets>,
+    signers: HashMap<String, Signers>,
+}
 
 impl KillSwitchSettings {
     pub(crate) async fn new() -> Result<Self> {
         // Get config
-        let config = if let Some(config_url) = std::env::var("CONFIG_URL").ok() {
-            nomad_xyz_configuration::NomadConfig::fetch(&config_url)
+        let config = if let Some(config_url) = env::var("CONFIG_URL").ok() {
+            NomadConfig::fetch(&config_url)
                 .await
                 .expect(&format!("Unable to load config from {}", config_url))
-        } else if let Some(config_path) = std::env::var("CONFIG_PATH").ok() {
-            nomad_xyz_configuration::NomadConfig::from_file(&config_path)
+        } else if let Some(config_path) = env::var("CONFIG_PATH").ok() {
+            NomadConfig::from_file(&config_path)
                 .expect(&format!("Unable to load config from {}", config_path))
         } else {
-            color_eyre::eyre::bail!(
+            eyre::bail!(
                 "No configuration found. Set CONFIG_URL or CONFIG_PATH environment variable"
             )
         };
@@ -22,6 +28,9 @@ impl KillSwitchSettings {
 
         //
 
-        return Ok(Self {});
+        return Ok(Self {
+            secrets: HashMap::new(),
+            signers: HashMap::new(),
+        });
     }
 }
