055d670f6aa7a620504824bacdd24fa6c8d3028d
Matthew Smith <m@lattejed.com>
2022-08-22 20:09:40 +0700

    Stub signed failure fn

--- tools/killswitch/src/killswitch.rs
+++ tools/killswitch/src/killswitch.rs
@@ -1,19 +1,35 @@
 use crate::{settings::KillSwitchSettings, Args};
 use color_eyre::Result;
+use ethers::core::types::Signature;
+use nomad_core::{FailureNotification, SignedFailureNotification};
 
 #[derive(Debug)]
 pub(crate) struct KillSwitch {}
 
 impl KillSwitch {
-    pub(crate) async fn from_settings(_settings: KillSwitchSettings) -> Result<Self> {
+    pub(crate) async fn new(_args: Args, _settings: KillSwitchSettings) -> Result<Self> {
         //
 
         return Ok(Self {});
     }
 
-    pub(crate) async fn run(&self, args: Args) -> Result<()> {
+    pub(crate) async fn run(&self) -> Result<()> {
         //
 
         return Ok(());
+    }
+
+    async fn create_signed_failure(&self) -> SignedFailureNotification {
+        SignedFailureNotification {
+            notification: FailureNotification {
+                home_domain: 0,
+                updater: Default::default(),
+            },
+            signature: Signature {
+                r: Default::default(),
+                s: Default::default(),
+                v: 0,
+            },
+        }
     }
 }
--- tools/killswitch/src/main.rs
+++ tools/killswitch/src/main.rs
@@ -1,15 +1,20 @@
 mod killswitch;
 mod settings;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use color_eyre::Result;
 use killswitch::KillSwitch;
 use settings::KillSwitchSettings as Settings;
 
+#[derive(ValueEnum, Clone, Debug)]
+enum App {
+    TokenBridge,
+}
+
 #[derive(Parser, Debug)]
 struct Args {
-    #[clap(short, long)]
-    app: String,
+    #[clap(short, long, arg_enum)]
+    app: App,
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -20,9 +25,9 @@ async fn main() -> Result<()> {
 
     let settings = Settings::new().await?;
 
-    let killswitch = KillSwitch::from_settings(settings).await?;
+    let killswitch = KillSwitch::new(args, settings).await?;
 
-    killswitch.run(args).await?;
+    killswitch.run().await?;
 
     Ok(())
 }
