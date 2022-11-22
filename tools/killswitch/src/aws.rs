013e5930d4dd49f73a6b517db63aad1a2a4a419b
Matthew Smith <m@lattejed.com>
2022-11-23 00:08:47 +0700

    Combine cred fetch and validate

--- tools/killswitch/src/aws.rs
+++ tools/killswitch/src/aws.rs
@@ -1,25 +1,14 @@
 use crate::{errors::Error, Result};
 use rusoto_core::credential::{ProfileProvider, ProvideAwsCredentials};
 
-/// An AWS credentials helper that checks for locally-stored
-/// credentials and validates them (as existing)
-pub(crate) struct MasterCredentials {
-    /// Internal `ProfileProvider` we use to handle local credentials
-    provider: ProfileProvider,
-}
+/// An AWS credentials helper that checks for locally-stored credentials
+pub(crate) struct AWSCredentials;
 
-impl MasterCredentials {
-    /// New `MasterCredentials` with locally stored credentials
-    pub(crate) fn new_from_credentials() -> Result<Self> {
-        Ok(MasterCredentials {
-            provider: ProfileProvider::new().map_err(Error::CredentialsError)?,
-        })
-    }
-
-    /// Ensure that the underlying provider can return valid credentials
-    pub(crate) async fn validate_credentials(&self) -> Result<()> {
-        let _ = self
-            .provider
+impl AWSCredentials {
+    /// Check for `AWSCredentials` stored locally
+    pub(crate) async fn check_credentials() -> Result<()> {
+        ProfileProvider::new()
+            .map_err(Error::CredentialsError)?
             .credentials()
             .await
             .map_err(Error::CredentialsError)?;
