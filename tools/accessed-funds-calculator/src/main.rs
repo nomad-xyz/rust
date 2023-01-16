f2ed2ccdaf538f4752d9a7edfe07dd37a8c7f4a9
Ryan Swanson <ryan@nomad.xyz>
2023-01-16 10:26:23 -0500

    cleaning up quite a bit

--- tools/accessed-funds-calculator/src/main.rs
+++ tools/accessed-funds-calculator/src/main.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
 use std::env;
 
-use serde_json;
-
 mod tokens;
 
 use crate::tokens::{Token, TokenName};
@@ -11,128 +9,114 @@ use crate::tokens::{Token, TokenName};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let usdc = Token {
         name: TokenName::USDC,
-        id: String::from("usd-coin"),
+        id: "usd-coin".to_string(),
         decimals: 6.0,
-        contract_address: String::from("0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"),
+        contract_address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48".to_string(),
         recovered_total: 12_890_538.932401,
-        currentPrice: 0.9992,
     };
 
     let cqt = Token {
         name: TokenName::CQT,
-        id: String::from("covalent"),
+        id: "covalent".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0xD417144312DbF50465b1C641d016962017Ef6240"),
+        contract_address: "0xD417144312DbF50465b1C641d016962017Ef6240".to_string(),
         recovered_total: 34_082_775.75159970,
-        currentPrice: 0.112701,
     };
 
     let usdt = Token {
         name: TokenName::USDT,
-        id: String::from("tether"),
+        id: "tether".to_string(),
         decimals: 6.0,
-        contract_address: String::from("0xdAC17F958D2ee523a2206206994597C13D831ec7"),
+        contract_address: "0xdAC17F958D2ee523a2206206994597C13D831ec7".to_string(),
         recovered_total: 4_673_863.595197,
-        currentPrice: 0.9994,
     };
 
     let wbtc = Token {
         name: TokenName::WBTC,
-        id: String::from("wrapped-bitcoin"),
+        id: "wrapped-bitcoin".to_string(),
         decimals: 8.0,
-        contract_address: String::from("0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599"),
+        contract_address: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599".to_string(),
         recovered_total: 280.73117399,
-        currentPrice: 18_805.78,
     };
 
     let frax = Token {
         name: TokenName::FRAX,
-        id: String::from("frax"),
+        id: "frax".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x853d955aCEf822Db058eb8505911ED77F175b99e"),
+        contract_address: "0x853d955aCEf822Db058eb8505911ED77F175b99e".to_string(),
         recovered_total: 2_644_469.91860909,
-        currentPrice: 1.001,
     };
 
     let iag = Token {
         name: TokenName::IAG,
-        id: String::from("iagon"),
+        id: "iagon".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x40EB746DEE876aC1E78697b7Ca85142D178A1Fc8"),
+        contract_address: "0x40EB746DEE876aC1E78697b7Ca85142D178A1Fc8".to_string(),
         recovered_total: 349_507_392.18740200,
-        currentPrice: 0.0054,
     };
 
     let weth = Token {
         name: TokenName::WETH,
-        id: String::from("weth"),
+        id: "weth".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"),
+        contract_address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".to_string(),
         recovered_total: 1_049.63562980,
-        currentPrice: 1425.31,
     };
 
     let dai = Token {
         name: TokenName::DAI,
-        id: String::from("dai"),
+        id: "dai".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x6B175474E89094C44Da98b954EedeAC495271d0F"),
+        contract_address: "0x6B175474E89094C44Da98b954EedeAC495271d0F".to_string(),
         recovered_total: 866_070.75687635,
-        currentPrice: 1.0,
     };
 
     let c3 = Token {
         name: TokenName::C3,
-        id: String::from("charli3"),
+        id: "charli3".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0xf1a91C7d44768070F711c68f33A7CA25c8D30268"),
+        contract_address: "0xf1a91C7d44768070F711c68f33A7CA25c8D30268".to_string(),
         recovered_total: 1_684_711.12239136,
-        currentPrice: 0.1673,
     };
 
     let fxs = Token {
         name: TokenName::FXS,
-        id: String::from("frax-share"),
+        id: "frax-share".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0"),
+        contract_address: "0x3432B6A60D23Ca0dFCa7761B7ab56459D9C964D0".to_string(),
         recovered_total: 46_895.68804450,
-        currentPrice: 5.93,
     };
 
     let cards = Token {
         name: TokenName::CARDS,
-        id: String::from("cardstarter"),
+        id: "cardstarter".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x3d6F0DEa3AC3C607B3998e6Ce14b6350721752d9"),
+        contract_address: "0x3d6F0DEa3AC3C607B3998e6Ce14b6350721752d9".to_string(),
         recovered_total: 165_005.81948028,
-        currentPrice: 0.2026,
     };
 
     let hbot = Token {
         name: TokenName::HBOT,
-        id: String::from("hummingbot"),
+        id: "hummingbot".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0xE5097D9baeAFB89f9bcB78C9290d545dB5f9e9CB"),
+        contract_address: "0xE5097D9baeAFB89f9bcB78C9290d545dB5f9e9CB".to_string(),
         recovered_total: 900_239.99796600,
-        currentPrice: 0.0086,
     };
 
     let sdl = Token {
         name: TokenName::SDL,
-        id: String::from("saddle-finance"),
+        id: "saddle-finance".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0xf1Dc500FdE233A4055e25e5BbF516372BC4F6871"),
+        contract_address: "0xf1Dc500FdE233A4055e25e5BbF516372BC4F6871".to_string(),
         recovered_total: 9_790.82405700,
-        currentPrice: 0.0,
     };
 
     let gero = Token {
         name: TokenName::GERO,
-        id: String::from("gerowallet"),
+        id: "gerowallet".to_string(),
         decimals: 18.0,
-        contract_address: String::from("0x3431F91b3a388115F00C5Ba9FdB899851D005Fb5"),
+        contract_address: "0x3431F91b3a388115F00C5Ba9FdB899851D005Fb5".to_string(),
         recovered_total: 23_245_641.66618310,
-        currentPrice: 0.0,
     };
 
     let address = "0xa4B86BcbB18639D8e708d6163a0c734aFcDB770c";
@@ -145,14 +129,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     for token in token_vec {
         let balance: f64 = get_token_balance(&token, address).await?;
         let accessed = token.recovered_total - balance;
-        total_accessed_value = total_accessed_value + (accessed * token.currentPrice);
-        println!("{}:{}", token.id, accessed);
+        let token_price = get_token_price(&token).await?;
 
-        // let token_price = get_token_price(&token).await?;
-        // println!("{:#?}", token_price["ethereum"]);
+        total_accessed_value = total_accessed_value + (accessed * token_price);
+        println!("{}:{} accessed, price: {}", token.id, accessed, token_price);
     }
 
-    println!("total accessed value: ${}", total_accessed_value);
+    println!("#################################################");
+    println!("#################################################");
+    println!("### total accessed value: ${} ###", total_accessed_value);
+    println!("#################################################");
+    println!("#################################################");
 
     Ok(())
 }
@@ -175,32 +162,25 @@ async fn get_token_balance(
         .await?;
     let balance: f64 = resp["result"].parse().unwrap();
     let denominator = (10.0_f64).powf(token.decimals);
-    // println!("balance: {}/{}", balance, denominator);
     let balance = balance / ((10.0_f64).powf(token.decimals));
 
     Ok(balance)
 }
 
-// async fn get_token_price(
-//     token: &Token,
-// ) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
-//     let coingecko_url = "https://api.coingecko.com/api/v3/simple/price";
-//     let ids = "ethereum";
-//     let vs_currency = "usd";
+async fn get_token_price(token: &Token) -> Result<f64, Box<dyn std::error::Error>> {
+    let coingecko_url = "https://api.coingecko.com/api/v3/simple/price";
+    let vs_currency = "usd";
 
-//     let request_url = format!(
-//         "{}?ids={}&vs_currencies={}",
-//         coingecko_url, ids, vs_currency
-//     );
+    let request_url = format!(
+        "{}?ids={}&vs_currencies={}",
+        coingecko_url, &token.id, vs_currency
+    );
+    let resp = reqwest::get(request_url)
+        .await?
+        .json::<HashMap<String, HashMap<String, f64>>>()
+        .await?;
+    let price = resp[&token.id].clone();
+    let price = price["usd"];
 
-//     let resp = reqwest::get(request_url)
-//     .await?
-//     .text()
-//     .await?;
-//     // let resp: &str = &resp.await?;
-//     let resp = serde_json::from_str(&resp)?;
-
-//     dbg!(&resp);
-//     // println!("{:?}",resp);
-//     Ok(resp)
-// }
+    Ok(price)
+}
--- tools/accessed-funds-calculator/src/tokens.rs
+++ tools/accessed-funds-calculator/src/tokens.rs
@@ -21,7 +21,6 @@ pub struct Token {
     pub name: TokenName,
     pub id: String,
     pub decimals: f64,
-    pub currentPrice: f64,
     pub contract_address: String,
     pub recovered_total: f64,
 }
